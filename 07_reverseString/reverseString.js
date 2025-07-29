48caedc87b882b242b21a4f0d5edb1318acdec94
Ateeksh <ateesood@gmail.com>
2025-07-29 14:30:20 +0000

    completed questtion seven

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let length = string.length;
 
+    if(length == 1){
+        return string;
+    }
+
+    let arr = string.split("");
+    let temp = '';
+
+    for(let i=0 ; i<length/2 ; i++){
+       
+        temp = arr[i];
+        arr[i] = arr[length - i - 1];
+        arr[length - i - 1] = temp; 
+    }
+
+    return arr.join("");
 };
 
 // Do not edit below this line
